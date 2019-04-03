a58a5bee599b3a9138f879272fea2010efccd511
Lumir Balhar <lbalhar@redhat.com>
2019-04-03 16:41:02 +0200

    Blacked code style

--- meetup_pyvo_pull.py
+++ meetup_pyvo_pull.py
@@ -17,7 +17,7 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 
 def get_meetup_data(group, meetup_id):
     # Get available data about meetup
-    url = 'https://api.meetup.com/{}/events/{}'.format(group, meetup_id)
+    url = "https://api.meetup.com/{}/events/{}".format(group, meetup_id)
     json_data = urlopen(url).read().decode()
     return json.loads(json_data)
 
@@ -28,34 +28,34 @@ def process_start(timestamp):
     timestamp = int(str(timestamp)[:10])
     dt = datetime.utcfromtimestamp(timestamp)
     dt = dt.replace(tzinfo=pytz.utc)
-    local_timezone = pytz.timezone('Europe/Prague')
+    local_timezone = pytz.timezone("Europe/Prague")
     localized = dt.astimezone(local_timezone)
-    return localized.strftime('%Y-%m-%d %H:%M:%S')
+    return localized.strftime("%Y-%m-%d %H:%M:%S")
 
 
 def process_description(description):
     # Process description from HTML to intended YAML
     output = []
-    description = re.sub('</p>', '\n\n', description)
-    description = re.sub('<[^<]+?>', '', description)
+    description = re.sub("</p>", "\n\n", description)
+    description = re.sub("<[^<]+?>", "", description)
     parts = description.splitlines()
-    indent = ' ' * 4
+    indent = " " * 4
     for part in parts:
         part = part.strip()
-        part = textwrap.fill(part, width=75,
-                             subsequent_indent=indent,
-                             initial_indent=indent)
+        part = textwrap.fill(
+            part, width=75, subsequent_indent=indent, initial_indent=indent
+        )
         output.append(part)
 
     del output[-1]
 
-    return '\n'.join(output)
+    return "\n".join(output)
 
 
 def load_and_fill_template(**kwargs):
     # Load YAML template and fill it with content of **kwargs
-    template_file = os.path.join(DIR, 'event.yaml.tpl')
-    with open(template_file, 'r') as fh:
+    template_file = os.path.join(DIR, "event.yaml.tpl")
+    with open(template_file, "r") as fh:
         template = fh.read()
 
     template = Template(template)
@@ -65,69 +65,94 @@ def load_and_fill_template(**kwargs):
 
 def remove_diacritic(input_str):
     # Remove diacritics from string
-    nfkd_form = unicodedata.normalize('NFKD', input_str)
-    only_ascii = nfkd_form.encode('ASCII', 'ignore')
+    nfkd_form = unicodedata.normalize("NFKD", input_str)
+    only_ascii = nfkd_form.encode("ASCII", "ignore")
     return only_ascii.decode()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     parser = ArgumentParser(
-        description='Create event based on meetup on meetup.com',
-        formatter_class=ArgumentDefaultsHelpFormatter
+        description="Create event based on meetup on meetup.com",
+        formatter_class=ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument('-c', '--city', dest='city', type=str, required=True,
-                        help='Name of the city')
-    parser.add_argument('-v', '--venue', dest='venue', type=str, required=True,
-                        help='Name of the venue in the city')
-    parser.add_argument('-s', '--serie', dest='serie', type=str, default=None,
-                        help='Name of the serie. {{city}}-pyvo if ommited')
-    parser.add_argument('-g', '--group', dest='group', type=str, required=True,
-                        help='Name of the group on meetup.com')
-    parser.add_argument('-i', '--id', dest='meetup_id', type=str,
-                        required=True, help='Id of the meetup on meetup.com')
+    parser.add_argument(
+        "-c", "--city", dest="city", type=str, required=True, help="Name of the city"
+    )
+    parser.add_argument(
+        "-v",
+        "--venue",
+        dest="venue",
+        type=str,
+        required=True,
+        help="Name of the venue in the city",
+    )
+    parser.add_argument(
+        "-s",
+        "--serie",
+        dest="serie",
+        type=str,
+        default=None,
+        help="Name of the serie. {{city}}-pyvo if ommited",
+    )
+    parser.add_argument(
+        "-g",
+        "--group",
+        dest="group",
+        type=str,
+        required=True,
+        help="Name of the group on meetup.com",
+    )
+    parser.add_argument(
+        "-i",
+        "--id",
+        dest="meetup_id",
+        type=str,
+        required=True,
+        help="Id of the meetup on meetup.com",
+    )
 
     args = parser.parse_args()
 
     # Check city
-    if not os.path.isdir(os.path.join(DIR, 'cities', args.city)):
-        print('City {} doesn\'t exist'.format(args.city))
+    if not os.path.isdir(os.path.join(DIR, "cities", args.city)):
+        print("City {} doesn't exist".format(args.city))
         sys.exit(1)
 
     # Check venue in the city
-    if not os.path.isfile(os.path.join(DIR, 'cities', args.city, 'venues',
-                          args.venue + '.yaml')):
-        print('Venue {} doesn\'t exist'.format(args.venue))
+    if not os.path.isfile(
+        os.path.join(DIR, "cities", args.city, "venues", args.venue + ".yaml")
+    ):
+        print("Venue {} doesn't exist".format(args.venue))
         sys.exit(1)
 
     # Construct serie name if arg is ommited
     if args.serie is None:
-        args.serie = args.city + '-pyvo'
-        print('Argument --serie is omitted, using {}'.format(args.serie))
-    if not os.path.isdir(os.path.join(DIR, 'series', args.serie)):
-        print('Serie {} doesn\'t exist'.format(args.serie))
+        args.serie = args.city + "-pyvo"
+        print("Argument --serie is omitted, using {}".format(args.serie))
+    if not os.path.isdir(os.path.join(DIR, "series", args.serie)):
+        print("Serie {} doesn't exist".format(args.serie))
         sys.exit(1)
 
     meetup_data = get_meetup_data(args.group, args.meetup_id)
     data = {
-        'city': args.city,
-        'start': process_start(meetup_data['time']),
-        'name': meetup_data['group']['name'],
-        'topic': meetup_data['name'],
-        'description': process_description(meetup_data['description']),
-        'venue': args.venue,
-        'url': meetup_data['link']
+        "city": args.city,
+        "start": process_start(meetup_data["time"]),
+        "name": meetup_data["group"]["name"],
+        "topic": meetup_data["name"],
+        "description": process_description(meetup_data["description"]),
+        "venue": args.venue,
+        "url": meetup_data["link"],
     }
 
     event_file_content = load_and_fill_template(**data)
-    event_date = data['start'].split()[0]
-    event_name = remove_diacritic(data['topic']).replace(' ', '-')
-    event_file_name = '{}-{}.yaml'.format(event_date, event_name)
+    event_date = data["start"].split()[0]
+    event_name = remove_diacritic(data["topic"]).replace(" ", "-")
+    event_file_name = "{}-{}.yaml".format(event_date, event_name)
 
-    dest_file_name = os.path.join(DIR, 'series', args.serie, 'events',
-                                  event_file_name)
+    dest_file_name = os.path.join(DIR, "series", args.serie, "events", event_file_name)
 
-    with open(dest_file_name, 'w') as fh:
-        print('Writing to', dest_file_name)
+    with open(dest_file_name, "w") as fh:
+        print("Writing to", dest_file_name)
         fh.write(event_file_content)
--- test_meetup_pyvo_pull.py
+++ test_meetup_pyvo_pull.py
@@ -1,92 +1,95 @@
 import pytest
-from meetup_pyvo_pull import (process_start, process_description,
-                              remove_diacritic, get_meetup_data,
-                              load_and_fill_template)
+from meetup_pyvo_pull import (
+    process_start,
+    process_description,
+    remove_diacritic,
+    get_meetup_data,
+    load_and_fill_template,
+)
 
 testdata = [
-    (1505209577, '2017-09-12 11:46:17'),
-    (15052095770000, '2017-09-12 11:46:17'),
-    (150520957700000000, '2017-09-12 11:46:17'),
-    (1505209671, '2017-09-12 11:47:51'),
-    ('1505209671', '2017-09-12 11:47:51')
+    (1505209577, "2017-09-12 11:46:17"),
+    (15052095770000, "2017-09-12 11:46:17"),
+    (150520957700000000, "2017-09-12 11:46:17"),
+    (1505209671, "2017-09-12 11:47:51"),
+    ("1505209671", "2017-09-12 11:47:51"),
 ]
 
 
-@pytest.mark.parametrize('timestamp, expected', testdata)
+@pytest.mark.parametrize("timestamp, expected", testdata)
 def test_process_start(timestamp, expected):
     assert process_start(timestamp) == expected
 
 
 testdata = [
-    ('</p>', ''),
-    ('blah', ''),
-    ('blah\nblah', '    blah'),
-    ('blah\nblah\nblah', '    blah\n    blah'),
-    ('blah</br><p><a href="#">blah</a></p>blah\nend',
-     '    blahblah\n\n    blah')
+    ("</p>", ""),
+    ("blah", ""),
+    ("blah\nblah", "    blah"),
+    ("blah\nblah\nblah", "    blah\n    blah"),
+    ('blah</br><p><a href="#">blah</a></p>blah\nend', "    blahblah\n\n    blah"),
 ]
 
 
-@pytest.mark.parametrize('description, expected', testdata)
+@pytest.mark.parametrize("description, expected", testdata)
 def test_process_description(description, expected):
     assert process_description(description) == expected
 
 
 testdata = [
-    ('', ''),
-    ('blah', 'blah'),
-    ('příšerně žluťoučký kůň pěl ďábelské ódy',
-     'priserne zlutoucky kun pel dabelske ody')
+    ("", ""),
+    ("blah", "blah"),
+    (
+        "příšerně žluťoučký kůň pěl ďábelské ódy",
+        "priserne zlutoucky kun pel dabelske ody",
+    ),
 ]
 
 
-@pytest.mark.parametrize('input, expected', testdata)
+@pytest.mark.parametrize("input, expected", testdata)
 def test_remove_diacritic(input, expected):
     assert remove_diacritic(input) == expected
 
 
 testdata = [
-    ('Ostravske-Pyvo',
-     '243107342',
-     'Ty nejlepší lightning talky',
-     'restaurace V.R. Levský',
-     'Ostravské Pyvo'
-     )
+    (
+        "Ostravske-Pyvo",
+        "243107342",
+        "Ty nejlepší lightning talky",
+        "restaurace V.R. Levský",
+        "Ostravské Pyvo",
+    )
     # TODO add one more meetup to test
 ]
 
 
-@pytest.mark.parametrize('group, meetup_id, name, venue_name, group_name',
-                         testdata)
+@pytest.mark.parametrize("group, meetup_id, name, venue_name, group_name", testdata)
 def test_get_meetup_data(group, meetup_id, name, venue_name, group_name):
     data = get_meetup_data(group, meetup_id)
-    assert data['name'] == name
-    assert data['venue']['name'] == venue_name
-    assert data['group']['name'] == group_name
-    assert data['link'].endswith('{}/events/{}/'.format(group, meetup_id))
+    assert data["name"] == name
+    assert data["venue"]["name"] == venue_name
+    assert data["group"]["name"] == group_name
+    assert data["link"].endswith("{}/events/{}/".format(group, meetup_id))
 
 
-@pytest.mark.parametrize('group, meetup_id, name, venue_name, group_name',
-                         testdata)
-def test_load_and_fill_template(group, meetup_id, name, venue_name,
-                                group_name):
+@pytest.mark.parametrize("group, meetup_id, name, venue_name, group_name", testdata)
+def test_load_and_fill_template(group, meetup_id, name, venue_name, group_name):
     meetup_data = get_meetup_data(group, meetup_id)
     data = {
-        'city': 'Ostrava',
-        'start': process_start(meetup_data['time']),
-        'name': meetup_data['group']['name'],
-        'topic': meetup_data['name'],
-        'description': process_description(meetup_data['description']),
-        'venue': 'V.R. Levský',
-        'url': meetup_data['link']
+        "city": "Ostrava",
+        "start": process_start(meetup_data["time"]),
+        "name": meetup_data["group"]["name"],
+        "topic": meetup_data["name"],
+        "description": process_description(meetup_data["description"]),
+        "venue": "V.R. Levský",
+        "url": meetup_data["link"],
     }
 
     result = load_and_fill_template(**data)
-    assert '{' not in result
-    assert '}' not in result
+    assert "{" not in result
+    assert "}" not in result
     for line in result.splitlines():
         line = line.strip()
-        if not line.startswith('urls'):
-            assert not line.endswith(':')
+        if not line.startswith("urls"):
+            assert not line.endswith(":")
         else:
-            assert line.endswith(':')
+            assert line.endswith(":")
